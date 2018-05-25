68f826f9b6c3ebaf71b3185bbd75aca2c7c4c1da
ktr0731 <aizu.s1230022@gmail.com>
2018-05-25 10:28:42 +0900

    add base JSON repository

---
+++ db.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+type DB struct {
+	Bookmarks []*Bookmark `json:"bookmarks"`
+}
+
+type Bookmark struct {
+	Name      string    `json:"name"`
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Repository struct {
+	Bookmark BookmarkRepository
+}
+
+type BookmarkRepository interface {
+	Add(context.Context, *Bookmark) error
+	List(context.Context) ([]*Bookmark, error)
+	Update(context.Context, *Bookmark) error
+	Delete(context.Context, *Bookmark) error
+}
+
+type jsonBookmarkRepository struct {
+	bookmarks []*Bookmark
+}
+
+func (j *jsonBookmarkRepository) Add(context.Context, *Bookmark) error {
+	panic("not implemented")
+}
+
+func (j *jsonBookmarkRepository) List(context.Context) ([]*Bookmark, error) {
+	panic("not implemented")
+}
+
+func (j *jsonBookmarkRepository) Update(context.Context, *Bookmark) error {
+	panic("not implemented")
+}
+
+func (j *jsonBookmarkRepository) Delete(context.Context, *Bookmark) error {
+	panic("not implemented")
+}
+
+func NewRepository() (*Repository, error) {
+	return newJSONRepository()
+}
+
+func newJSONRepository() (*Repository, error) {
+	f, err := os.Open("in.json")
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load JSON database")
+	}
+	defer f.Close()
+
+	var db DB
+	if err := json.NewDecoder(f).Decode(&db); err != nil {
+		return nil, errors.Wrap(err, "failed to decode JSON database")
+	}
+
+	return &Repository{
+		Bookmark: &jsonBookmarkRepository{bookmarks: db.Bookmarks},
+	}, nil
+}
