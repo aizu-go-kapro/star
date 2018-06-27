8d2705bb4cdc27eb703c1146a77eb7c7e52b3f5a
slme9364 <ishikazu1104@yahoo.co.jp>
2018-06-27 18:37:37 +0900

    Implement Add command Run

--- add.go
+++ add.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/mitchellh/cli"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/mitchellh/cli"
+)
 
 type AddCommand struct {
 }
@@ -10,7 +16,18 @@ func (a *AddCommand) Help() string {
 }
 
 func (a *AddCommand) Run(args []string) int {
-	panic("not implemented")
+	if len(args) != 2 {
+		fmt.Println("Not match arguments")
+		return 1
+	}
+
+	bookmark := &Bookmark{Name: args[0], URL: args[1], CreatedAt: time.Now()}
+	if err := repo.Bookmark.Add(context.TODO(), bookmark); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	return 0
 }
 
 func (a *AddCommand) Synopsis() string {
