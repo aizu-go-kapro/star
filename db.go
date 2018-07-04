d97dcfb314f9ab8f0f9fdd55c3914460e6029477
DESKTOP-03PI5PC\ryu19 <ryu1998tea@gmail.com>
2018-07-04 18:55:41 +0900

    alrady -> already

--- db.go
+++ db.go
@@ -51,7 +51,7 @@ type jsonBookmarkRepository struct {
 func (j *jsonBookmarkRepository) Add(ctx context.Context, b *Bookmark) error {
 	for _, e := range j.bookmarks {
 		if e.Name == b.Name {
-			return errors.New("Alrady exist bookmark name")
+			return errors.New("Already exist bookmark name")
 		}
 	}
 	j.bookmarks = append(j.bookmarks, b)
