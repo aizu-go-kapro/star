09a0ad4bb0cbb9a7217e8babad98ff6180f95255
slme9364 <ishikazu1104@yahoo.co.jp>
2018-07-11 17:48:25 +0900

    [WIP] Fix add command using args and return function

--- add.go
+++ add.go
@@ -22,11 +22,12 @@ func (a *AddCommand) Run(args []string) int {
 		return 1
 	}
 
-	if _, err := url.ParseRequestURI(args[1]); err != nil {
+	if _, err := url.ParseRequestURI(args[0]); err != nil {
 		fmt.Println(err)
+		return 1
 	}
 
-	bookmark := &Bookmark{Name: args[0], URL: args[1], CreatedAt: time.Now()}
+	bookmark := &Bookmark{Name: args[1], URL: args[0], CreatedAt: time.Now()}
 	if err := repo.Bookmark.Add(context.Background(), bookmark); err != nil {
 		fmt.Println(err)
 		return 1
