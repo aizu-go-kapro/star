d2d78237339d9f44292b6da0bc227124587176ba
ktr0731 <aizu.s1230022@gmail.com>
2018-07-13 02:21:48 +0900

    refactor db_test.go

--- db_test.go
+++ db_test.go
@@ -12,6 +12,10 @@ func TestJSONBookmark(t *testing.T) {
 	b := &Bookmark{Name: "bookmark", URL: "http://example.com", CreatedAt: time.Now()}
 	repo := &jsonBookmarkRepository{}
 
+	cleanup := func(t *testing.T) {
+		repo = &jsonBookmarkRepository{}
+	}
+
 	assertBookmarkLen := func(t *testing.T, expected int) {
 		if len(repo.bookmarks) != expected {
 			t.Errorf("expected number of %d bookmarks, but got %d", expected, len(repo.bookmarks))
@@ -27,20 +31,10 @@ func TestJSONBookmark(t *testing.T) {
 		}
 	}
 
-	updateBookmark := func(t *testing.T, b ...*Bookmark) {
-		for _, bookmark := range b {
-			err := repo.Update(context.TODO(), bookmark)
-			if err != nil {
-				t.Errorf("expected nil, but got error: %s", err)
-			}
-		}
-	}
-
 	t.Run("Add adds a bookmark to the repository", func(t *testing.T) {
-		assertBookmarkLen(t, 0)
+		defer cleanup(t)
 
 		addBookmarks(t, b)
-
 		assertBookmarkLen(t, 1)
 	})
 
@@ -54,56 +48,42 @@ func TestJSONBookmark(t *testing.T) {
 	// 		t.Errorf("expected one bookmark, but got %d bookmarks", len(bookmarks))
 	// 	}
 	// })
-	t.Run("Update updates one bookmark from passed key (name)", func(t *testing.T) {
+
+	t.Run("Update fails if the value corresponding to passed key is not found", func(t *testing.T) {
+		defer cleanup(t)
+
 		err := repo.Update(context.Background(), b)
+		if err == nil {
+			t.Fatal("expected an error, but got no errors")
+		}
+	})
+
+	t.Run("Update updates one bookmark from passed key (name)", func(t *testing.T) {
+		defer cleanup(t)
+
+		addBookmarks(t, b)
+		assertBookmarkLen(t, 1)
+
+		b2 := b
+		b.CreatedAt = time.Now()
+		b.URL = "https://updated-example.com"
+
+		err := repo.Update(context.Background(), b2)
 		if err != nil {
 			t.Fatalf("expected no errors, but got an error: %s", err)
 		}
 
-		assertBookmarkLen(t, 0)
+		actual := repo.bookmarks[0]
+		if *b2 != *actual {
+			t.Errorf("expected that Update updates b by b2 (%v), but not equal (%v)", b2, actual)
+		}
 	})
-	t.Run("ブックマークの要素数が変わっていないか確認", func(t *testing.T) {
-		b2 := &Bookmark{Name: "bookmark2", URL: "http://foo.com", CreatedAt: time.Now()}
-		assertBookmarkLen(t, 0)
+
+	t.Run("Delete deletes one bookmark from passed key (name)", func(t *testing.T) {
+		defer cleanup(t)
 
 		addBookmarks(t, b)
 
-		updateBookmark(t, b2)
-
-		assertBookmarkLen(t, 1)
-	})
-	t.Run("ブックマークの内容が変わっているか確認", func(t *testing.T) {
-		b2 := &Bookmark{Name: "bookmark2", URL: "http://foo.com", CreatedAt: time.Now()}
-		b3 := &Bookmark{Name: "bookmark3", URL: "http://bar.com", CreatedAt: time.Now()}
-		b4 := &Bookmark{Name: "bookmark4", URL: "http://hoge.com", CreatedAt: time.Now()}
-		assertBookmarkLen(t, 0)
-
-		addBookmarks(t, b, b2, b3)
-
-		Rebookmarks := repo.bookmarks
-
-		updateBookmark(t, b4)
-
-		var count = 2
-
-		for i, b := range repo.bookmarks {
-			if b.Name == Rebookmarks[i].Name {
-				count--
-			}
-		}
-		if count != 0 {
-			t.Fatalf("expected the others are same, but exist not same")
-		}
-		for i, b := range repo.bookmarks {
-			if b.Name == b4.Name {
-				break
-			}
-			if i == len(repo.bookmarks)-1 {
-				t.Fatalf("expected complete changing, but couldn't change: %s", b4.Name)
-			}
-		}
-	})
-	t.Run("Delete deletes one bookmark from passed key (name)", func(t *testing.T) {
 		t.Run("delete one bookmark from the repository which has only one bookmark", func(t *testing.T) {
 			err := repo.Delete(context.Background(), b)
 			if err != nil {
